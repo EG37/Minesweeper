3698b688942462a69ce73d66d17b321867a10a23
User <elgareew@yandex.ru>
2021-01-05 15:16:38 +0500

    Добавил шрифт, добавил возможность отображения текста на кнопке

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pygame
 
 
 class Button:
-    def __init__(self, x, y, width, height, on_click=lambda x: None):
+    def __init__(self, x, y, width, height, text='', on_click=lambda x: None):
         self.rect = pygame.rect.Rect(x, y, width, height)
         self.state = 'up'
+        self.text = text
         self.on_click = on_click
         self.colors = {
             'up': (225, 225, 225),
@@ -31,13 +32,19 @@ class Button:
     def draw(self):
         pygame.draw.rect(screen, self.colors[self.state], self.rect)
         pygame.draw.rect(screen, (0, 0, 0), self.rect, width=1, border_radius=3)
+        if self.text:
+            text = FONT.render(self.text, True, (0, 0, 0))
+            text_x = self.rect.x + self.rect.width // 2 - text.get_width() // 2
+            text_y = self.rect.y + self.rect.height // 2 - text.get_height() // 2
+            screen.blit(text, (text_x, text_y))
 
 
 SIZE = WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
-test_button = Button(10, 10, 100, 100, on_click=lambda x: print('test'))
+FONT = pygame.font.Font('pixel_font.otf', 22)
+test_button = Button(10, 10, 100, 100, 'test', on_click=lambda x: print('test'))
 
 
 if __name__ == '__main__':
